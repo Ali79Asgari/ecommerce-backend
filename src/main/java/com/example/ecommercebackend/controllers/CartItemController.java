4c085195e3338bd476826f510b89cd6c592d8af6
Ali <ali79asgari@gmail.com>
2024-09-22 12:17:07 +0330

    feat: add "/api" to all urls in controller classes

--- src/main/java/com/example/ecommercebackend/controllers/CartItemController.java
+++ src/main/java/com/example/ecommercebackend/controllers/CartItemController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cart-items")
+@RequestMapping("/api/cart-items")
 @RequiredArgsConstructor
 public class CartItemController {
 
